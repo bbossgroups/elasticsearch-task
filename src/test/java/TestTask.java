191f1ff36637950f7f6fe4147d0f075b27b1e9ee
yinbp <yin-bp@163.com>
2017-08-12 21:11:48 +0800

    清理文件
    清理文件

--- src/test/java/TestTask.java
+++ src/test/java/TestTask.java
@@ -1,8 +1,8 @@
 import org.frameworkset.task.TaskService;
 
 public class TestTask {
-    public static void main(String[] args){
+    public static void main(String[] args) throws Exception {
         TaskService.getTaskService().startService();
-
+        //HttpRequestUtil.httpGetforString("http://10.154.50.239:9200/");
     }
 }
